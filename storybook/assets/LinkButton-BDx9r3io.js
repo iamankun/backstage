d40fc0a1228788e05d4791f139204c2d866e9b90
awanlin <67169551+awanlin@users.noreply.github.com>
2025-05-02 20:49:32 +0000

    Deploying to gh-pages from @ backstage/backstage@8c5f8fe8d4a390042e9211a44151a439b2c6f980 🚀

---
+++ storybook/assets/LinkButton-BDx9r3io.js
@@ -0,0 +1,4 @@
+import{j as n}from"./jsx-runtime-CvpxdxdE.js";import{r}from"./index-DSHF18-l.js";import{L as i}from"./Link-C9d1VBt_.js";import{B as s}from"./Button-COCjtST4.js";const p=r.forwardRef((o,t)=>n.jsx(i,{ref:t,...o,color:"initial"})),m=r.forwardRef((o,t)=>n.jsx(s,{ref:t,component:p,...o}));m.__docgenInfo={description:`Thin wrapper on top of material-ui's {@link https://v4.mui.com/components/buttons/ | Button} component
+
+@public
+@remarks`,methods:[],displayName:"LinkButton"};export{m as L};
